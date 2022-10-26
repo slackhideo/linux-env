9783a0ea74b48646ab7369cd4203752cf43deef6
Tiago Barros <burajirugaijin@yahoo.co.jp>
2022-10-26 18:20:11 -0300

    Fixing bug (find iteration pitfall). New code uses bash instead of sh.

--- convert_encoding.sh
+++ convert_encoding.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 convert() {
     local dir="$1" encoding_new="$2"
-    for I in $(find "$dir"); do
+    while IFS= read -r -d '' I; do
         encoding_orig=$(file -bi "$I" | cut -d= -f2)
         if [ "$encoding_orig" != "binary" -a "$encoding_orig" != "utf-8" ]; then
             mv -i "$I" "$I".tmp
             iconv -f "$encoding_orig" -t "$encoding_new" -o "$I" "$I".tmp
             rm "$I".tmp
         fi
-    done
+    done < <(find "$dir" -print0)
 }
 convert "${1:-.}" "${2:-utf8}"
